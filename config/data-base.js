7a0315b6bf4e615abb4b5be653c79efd02272010
clair <armistead2000@gmail.com>
2022-09-13 14:37:39 -0500

    added basic html for /

--- config/data-base.js
+++ config/data-base.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
- 
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000
     })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (err) {
-    console.error(err)
-    process.exit(1)
-  }
+    if (err) return console.error("Error: ", err)
+    console.log(
+      "MongoDB Connection -- Ready state is:",
+      mongoose.connection.readyState) 
+    }
 }
 
 module.exports = connectDB
--- server.js
+++ server.js
@@ -28,7 +28,10 @@ app.use(compression()); //Compress all routes
 
 //routes go here
 app.use('/', routes); //to use the routes
-
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/index.html');
+});
 
 
 
