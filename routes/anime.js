c727a1503d194aad338c59105a85a867b4342c08
clair <armistead2000@gmail.com>
2022-10-01 21:35:16 -0500

    hot fix

--- routes/anime.js
+++ routes/anime.js
@@ -1,13 +1,9 @@
 const express = require('express'); //import express
 const router  = express.Router(); 
 const animeController = require('../controllers/anime'); 
-<<<<<<< HEAD
 const homeController = require("../controllers/home");
 
 router.get("/", homeController.getIndex);
-=======
-
->>>>>>> 6451f0b39d6d9cf66c4ba75f0659836f4208d27e
 router.get('/anime', animeController.getAllAnime);
 router.post('/anime', animeController.uploadImg, animeController.newAnime); 
 router.delete('/anime', animeController.deleteAllAnime);
--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 const express = require('express')
-<<<<<<< HEAD
 const app = express();
 const logger = require("morgan");
 const connectDB = require('./config/data-base')
 // const helmet = require('helmet');  erase if content security issue dissapears
-=======
-
-const app = express();
-
-const connectDB = require('./config/data-base')
-
-
-const helmet = require('helmet');
-
->>>>>>> 6451f0b39d6d9cf66c4ba75f0659836f4208d27e
 const routes = require('./routes/anime'); 
 const compression = require('compression');
 //Load config
@@ -23,15 +12,11 @@ require('dotenv').config({path: './config/.env'})
 connectDB()
 
 
-<<<<<<< HEAD
 //Using EJS for views
 app.set("view engine", "ejs");
 
 //Static Folder
 app.use(express.static("public"));
-=======
-
->>>>>>> 6451f0b39d6d9cf66c4ba75f0659836f4208d27e
 
 // makes uploads folder available
 app.use('/uploads', express.static('./uploads'));
@@ -39,26 +24,15 @@ app.use('/uploads', express.static('./uploads'));
 //Body parser (how the information is  received/formatted)
 app.use(express.json())
 
-<<<<<<< HEAD
 //Logging
 app.use(logger("dev"));
 
 // app.use(helmet()); erase if content security issue dissapears 
-=======
-app.use(helmet());
->>>>>>> 6451f0b39d6d9cf66c4ba75f0659836f4208d27e
 app.use(compression()); //Compress all routes
 
 //routes go here
 app.use('/', routes); //to use the routes
-<<<<<<< HEAD
 
-=======
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/index.html');
-});
->>>>>>> 6451f0b39d6d9cf66c4ba75f0659836f4208d27e
 
 
 
